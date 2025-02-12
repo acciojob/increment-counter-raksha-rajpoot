1124dc11437081ddce3083016e2d8aedf1b1a14b
raksha-rajpoot <rakraj654@gmail.com>
2025-02-12 18:00:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+ <script>
+        let counterValue = 0;
+        const counter = document.getElementById("counter");
+        const incrementBtn = document.getElementById("incrementBtn");
+
+        incrementBtn.addEventListener("click", function () {
+            alert(counterValue);
+            counterValue++;
+            counter.textContent = counterValue;
+        });
+    </script>
